d02bf7baf3f324be4c4c1b492cea737810dab44e
Santiago Zarate <santiago2001zarate@gmail.com>
2024-03-30 20:49:41 -0300

    chore: add basic testing setup

---
+++ e2e/App.test.tsx
@@ -0,0 +1,3 @@
+import { describe, test, should, beforeAll } from "vitest";
+import { render, cleanup } from "@testing-library/react";
+import { UserEvent } from "@testing-library/user-event";
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'node:path'
+/// <reference types="vitest" />
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "node:path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  test: {
+    environment: "happy-dom",
+  },
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src")
-    }
-  }
-})
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
